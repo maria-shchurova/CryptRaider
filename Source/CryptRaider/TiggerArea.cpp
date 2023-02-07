246186251e1d589233b9059b579764e9295508b0
Maria Shchurova <mary.shchurova@gmail.com>
2023-02-07 23:38:34 +0100

    TArray

--- Source/CryptRaider/TiggerArea.cpp
+++ Source/CryptRaider/TiggerArea.cpp
@@ -5,19 +5,24 @@
 
 UTiggerArea::UTiggerArea()
 {
-    PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = true;    
 }
 
 void UTiggerArea::BeginPlay()
 {
 	Super::BeginPlay();
-    UE_LOG(LogTemp, Display, TEXT("Trigger Component reports duty"));
 }
 
 void UTiggerArea::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
-    UE_LOG(LogTemp, Display, TEXT("Tick Component reports duty"));
 
+    TArray<AActor*> Actors;
+    GetOverlappingActors(Actors);
+
+    if(Actors.Num() > 0)
+    {
+        UE_LOG(LogTemp, Display, TEXT("Fisr Overlapped Actor is %s"), *Actors[0]->GetActorNameOrLabel());
+    }
 }
 
